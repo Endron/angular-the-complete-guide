08290bf2fd7ffc47ac870bd8548b62740b6a21bc
Tobias Kremer <t.kremer@reply.de>
2020-08-10 16:09:21 +0200

    first assignment

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warnint-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.scss']
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
